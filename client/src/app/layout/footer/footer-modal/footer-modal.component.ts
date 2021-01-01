7b5fa457f9f79e9d7b4afa2e976790a186505167
jsheridanwells <jsheridanwells@gmail.com>
2021-01-01 15:46:05 -0600

    auth page okay for now

--- client/src/app/layout/footer/footer-modal/footer-modal.component.ts
+++ client/src/app/layout/footer/footer-modal/footer-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -6,7 +6,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './footer-modal.component.html',
   styleUrls: ['./footer-modal.component.scss']
 })
-export class FooterModalComponent implements OnInit {
+export class FooterModalComponent {
 
   constructor(
     public activeModal: NgbActiveModal
