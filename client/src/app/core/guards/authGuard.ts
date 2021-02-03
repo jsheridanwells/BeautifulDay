490e6fdfdcf49b0133661a7b392813f8db3e1594
jsheridanwells <jsheridanwells@gmail.com>
2021-02-03 14:59:44 -0600

    save ui work

--- client/src/app/core/guards/authGuard.ts
+++ client/src/app/core/guards/authGuard.ts
@@ -1,27 +1,35 @@
 import { Injectable } from "@angular/core";
 import { CanActivate, Router } from "@angular/router";
 import { GoogleService } from "../services/google.service";
+import { AuthService }from '../services/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private googleService: GoogleService,
+    private authService: AuthService,
     private router: Router
   ) {  }
 
   canActivate(): boolean | Promise<boolean>  {
     const session = this.googleService.getToken();
+    const token = this.authService.getJwt();
     console.log('authguard check session', session);
-    if (!session) {
-      this.router.navigate(['/signin']);
+    if (!session || !token) {
+      this.handleReroute().then(() => false);
     }
     return this.googleService.checkSignedIn()
       .then(signedIn => {
         if (!signedIn) {
-          this.router.navigate(['/signin']);
-          return false;
+          this.handleReroute().then(() => false);
         }
         return true;
       });
   }
+
+  private async handleReroute(): Promise<any> {
+    await this.googleService.signOut();
+    this.router.navigate(['/signing']);
+  }
 }
+
